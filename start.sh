4903dcb287d62ab8a7f0ed47a337c1590857a9b2
h44343880 <h0920185003@gmail.com>
2024-09-05 18:07:26 +0800

    feat: add pr template and start service script

---
+++ start.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+echo "source /mnt/Nami/users/Jason0411202/anaconda3/bin/activate"
+source /mnt/Nami/users/Jason0411202/anaconda3/bin/activate
+echo "conda activate GPTSoVits"
+conda activate GPTSoVits
+echo "python pure_api.py"
+python pure_api.py
